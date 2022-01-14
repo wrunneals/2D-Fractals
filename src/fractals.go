663aae9311d93f3d3160d566fff41789c9aab706
wrunneals <88123539+wrunneals@users.noreply.github.com>
2022-01-14 13:52:06 -0800

    Completed go thread pool structure for multithreading pixel rendering. Added aspect ratio.

--- src/fractals.go
+++ src/fractals.go
@@ -8,19 +8,33 @@ import(
 	"os"
 )
 
-var resX int = 400
-var resY int = 400
+const resX int = 1920
+const resY int = 1080
+const numWorkers = 10
 
-var viewWidth float64 = 2.0
-var viewHeight float64 = 2.0
-
+var aspectRatio float64 = float64(resX) / float64(resY)
+var scale float64 = 2.0
 var center complex128 = -0.5 + 0i
 
+type PixelJob struct{
+	x int
+	y int
+}
+
+type PixelResult struct{
+	x int
+	y int
+	value float64
+}
+
 func abs(n complex128) float64{
+	/* Returns the absolute value of a complex number */
 	return math.Sqrt(real(n) * real(n) + imag(n) * imag(n))
 }
 
+
 func iteratePixel(x, y int) float64{
+	/* 	Iterates over z = z^2 + c and returns the escape value given a pixel point (x, y) */
 	maxIter := 100
 	z := 0 + 0i
 	c := mapPixel(x, y)
@@ -31,21 +45,43 @@ func iteratePixel(x, y int) float64{
 	return float64(i) / float64(maxIter)
 }
 
+
 func mapPixel(x, y int) complex128{
-	r := (real(center) - viewWidth / 2.0) + float64(x) / float64(resX) * viewWidth
-	i := (imag(center) - viewHeight / 2.0) + float64(y) / float64(resY) * viewHeight
+	/* Maps a pixel to a point on the complex plane given a center and view */
+	r := (real(center) - scale * aspectRatio / 2.0) + float64(x) / float64(resX) * scale * aspectRatio
+	i := (imag(center) - scale / 2.0) + float64(y) / float64(resY) * scale
 	return complex(r, i)
+}
+
+
+func worker(jobs <-chan PixelJob, results chan <- PixelResult){
+	/* Worker function for creating a worker pool to render indvidual pixels */
+	for job := range jobs{
+		value := iteratePixel(job.x, job.y)
+		results <- PixelResult{job.x, job.y, value}
+	}
 }
 
 func main(){
 	imgOut := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{resX, resY}})
-	
+	const numJobs = resX * resY
+	jobsChan := make(chan PixelJob, numJobs)
+	resultsChan := make(chan PixelResult, numJobs)
+
+	for i := 0; i < numWorkers; i ++{
+		go worker(jobsChan, resultsChan)
+	}
 	for x := 0; x < resX; x ++{
 		for y := 0; y < resY; y ++{
-			esc := uint8(iteratePixel(x, y) * 255)
-			c := color.RGBA{esc, esc, esc, 255}
-			imgOut.Set(x, y, c)
+			jobsChan <- PixelJob{x, y}
 		}
+	}
+	close(jobsChan)
+	for i := 0; i < numJobs; i ++{
+		result := <- resultsChan
+		esc := uint8(result.value * 255.0)
+		c := color.RGBA{esc, esc, esc, 255.0}
+		imgOut.Set(result.x, result.y, c)
 	}
 
 	f, _ := os.Create("../output.png")
